cf944af3a3dd5402cfef6d0f3de093193a18ca7c
omalleyjg <omalley.jg@gmail.com>
2023-02-01 16:52:09 -0400

    added javascript funtionality, still needs timer, stll needs question answers, still needs highscores.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,75 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-startButton.addEventListener('click', startGame)
+let shuffleQuestions, currentQuestionIndex;
+
+
+startButton.addEventListener('click', startGame, startTimer)
 
 //1. start game
-function startGame(){
-console.log('started');
-startButton.classList.add('hide');
+function startGame() {
+    console.log('started');
+    startButton.classList.add('hide');
+    shuffleQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 }
+
 //2. start countdown timer
+function startTimer(){
+
+}
 //  a. timer takes away time for wrong answer
 //  b. displays game over message when timer reaches zero
 //3. display question with answers
+function setNextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+questionElement.innerText = question.question;
+}
+
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        question: "Which built-in method removes the last element from an array and returns that element?",
+        choices: ["last()", "get()", "pop()", "None of the Above"],
+        answer: "pop()"
+    },
+    {
+        question: "Which built-in method returns the calling string value converted to lower case?",
+        choices: ["toLowerCase()", "toLower()", "changeCase(case)", "None of the Above"],
+        answer: "toLowerCase()"
+    },
+    {
+        question: "Which of the following function of Number object returns the number's value",
+        choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+        answer: "valueOf()"
+    },
+    {
+        question: "Which of the following function of Array object joins all elements of an array into a string?",
+        choices: ["concat()", "join()", "pop()", "map()"],
+        answer: "join()"
+    }
+];
+
+
+function selectAnswer() {
+
+}
 //  a. after question is answered correctly display next question
 //  b. repeat last function until game is complete
 //4. after game is finished allow user to save initials and view highscores
-
